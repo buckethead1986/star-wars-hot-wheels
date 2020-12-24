1e76ef76ec5b9d132fdd2cb5ca7fae393b85c21e
buckethead1986 <rspeckpfs@gmail.com>
2020-12-23 21:57:41 -0800

    copied over starWarsGridItem.js

---
+++ src/components/StarWarsGridItem.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    dataAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  image: {
+    width: 128,
+    height: 128
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
+  }
+}));
+
+export default function NestedGrid(props) {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <Grid>
+        <Paper className={classes.paper}>
+          <Grid container spacing={1}>
+            <Grid item xs={4}>
+              <ButtonBase className={classes.image}>
+                <img
+                  className={classes.img}
+                  alt="complex"
+                  src={props.ship.src}
+                />
+              </ButtonBase>
+            </Grid>
+            <Grid item xs={4} container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography>{props.ship.name}</Typography>
+              </Grid>
+              <Grid item xs>
+                <Typography>Year: {props.ship.year}</Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={4} container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography>Model: {props.ship.model}</Typography>
+              </Grid>
+              <Grid item xs>
+                <Typography>Sold as: {props.ship.pack} pack</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </React.Fragment>
+  );
+}
