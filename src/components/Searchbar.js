80f5276ba2f7b25a12fb8624580d3ba8cbbc0ffd
buckethead1986 <rspeckpfs@gmail.com>
2020-12-27 19:36:10 -0800

    fixed syntax

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -24,32 +24,32 @@ const useStyles = makeStyles(theme => ({
       borderColor: theme.palette.primary.main
     }
   },
-  focused: {},
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 1),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch"
-      }
-    }
-  }
+  focused: {}
+  // searchIcon: {
+  //   padding: theme.spacing(0, 2),
+  //   height: "100%",
+  //   position: "absolute",
+  //   pointerEvents: "none",
+  //   display: "flex",
+  //   alignItems: "center",
+  //   justifyContent: "center"
+  // },
+  // inputRoot: {
+  //   color: "inherit"
+  // },
+  // inputInput: {
+  //   padding: theme.spacing(1, 1, 1, 1),
+  //   // vertical padding + font size from searchIcon
+  //   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+  //   transition: theme.transitions.create("width"),
+  //   width: "100%",
+  //   [theme.breakpoints.up("sm")]: {
+  //     width: "12ch",
+  //     "&:focus": {
+  //       width: "20ch"
+  //     }
+  // }
+  // }
 }));
 
 const searchbarRegex = string => {
