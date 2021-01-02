f921b8b859a4a964baca219ec8d1d17221824195
buckethead1986 <rspeckpfs@gmail.com>
2021-01-01 19:38:48 -0800

    cleaned up searchbar, removed unused classes in makeStyles

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { fade, makeStyles, useTheme } from "@material-ui/core/styles"; //fade never used, remove in final
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import DrawerList from "./DrawerList.js";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -79,45 +79,6 @@ const useStyles = makeStyles(theme => ({
       display: "block"
     }
   }
-  // search: {
-  //   position: "relative",
-  //   borderRadius: theme.shape.borderRadius,
-  //   backgroundColor: fade(theme.palette.common.white, 0.15),
-  //   "&:hover": {
-  //     backgroundColor: fade(theme.palette.common.white, 0.25)
-  //   },
-  //   marginRight: 0,
-  //   width: "100%",
-  //   [theme.breakpoints.up("sm")]: {
-  //     marginLeft: theme.spacing(1),
-  //     width: "auto"
-  //   }
-  // }
-  // searchIcon: {
-  //   padding: theme.spacing(0, 2),
-  //   height: "100%",
-  //   position: "absolute",
-  //   pointerEvents: "none",
-  //   display: "flex",
-  //   alignItems: "center",
-  //   justifyContent: "center"
-  // },
-  // inputRoot: {
-  //   color: "inherit"
-  // },
-  // inputInput: {
-  //   padding: theme.spacing(1, 1, 1, 0),
-  //   // vertical padding + font size from searchIcon
-  //   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-  //   transition: theme.transitions.create("width"),
-  //   width: "100%",
-  //   [theme.breakpoints.up("sm")]: {
-  //     width: "12ch",
-  //     "&:focus": {
-  //       width: "20ch"
-  //     }
-  //   }
-  // }
 }));
 
 export default function ResponsiveDrawer(props) {
