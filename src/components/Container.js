54909888d81f3cf830399752ef3b93a69d1179fe
buckethead1986 <rspeckpfs@gmail.com>
2021-01-30 23:55:58 -0800

    finally figured out filtering for factions based on partial matches for an array of values

--- src/components/Container.js
+++ src/components/Container.js
@@ -10,24 +10,57 @@ export default function Container() {
       Speeder: false,
       Fighter: false,
       "X-Wing": false,
-      "TIE Fighter": false
-    }
+      "TIE Fighter": false,
+      "2016": true,
+      "2017": true,
+      "2018": false,
+      "2019": true,
+      "2020": false,
+      Imperial: true,
+      Rebel: false,
+      "Bounty Hunter": false,
+      Smuggler: false
+    },
+    currentFilters: []
   });
+  const filterFunctions = {
+    Imperial: ship => ship.faction.indexOf("Imperial") !== -1,
+    rebel: ship => ship.faction.indexOf("Rebel") !== -1
+    //...add more based on desired filters, then filter starWarsShips with .some(), iterating through the selected filters.
+  };
 
   const handleSearchbarData = searchbarData => {
     setSearchbarValue(searchbarData !== undefined ? searchbarData : "");
   };
   const handleShipFilter = shipFilterData => {
+    // result = ships.filter(item => {
+    //   return selected.some(f => f(item))
+    //
+    // }
+
     let shipFilterCopy = { ...shipFilter.filterParameters };
     // console.log(shipFilterCopy["Walker"], shipFilterData);
+    console.log("undefined?", shipFilterData);
     if (shipFilterData !== undefined) {
       shipFilterCopy[shipFilterData]
         ? (shipFilterCopy[shipFilterData] = false)
         : (shipFilterCopy[shipFilterData] = true);
     }
-    console.log(shipFilterCopy);
+    let newFilters = Object.keys(shipFilterCopy).filter(k => shipFilterCopy[k]);
+    console.log(newFilters);
+    newFilters = newFilters.map(el => filterFunctions[el.toLowerCase()]);
+    console.log(newFilters);
+    // const result = ships.filter(item => newFilters.some(f => f(item)));
+    // console.log(result);
 
-    // setShipFilter(shipFilterData !== undefined ? shipFilterData : "");
+    setShipFilter(
+      {
+        ...shipFilter,
+        filterParameters: shipFilterCopy,
+        currentFilters: newFilters
+      },
+      console.log("shipfilters", shipFilter, newFilters)
+    );
   };
 
   return (
